f03387a72bc5a5208796a2f8f5001e7b63ac71e8
Will B <willhbjr@gmail.com>
2021-02-18 08:40:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let food = "water bottles,meal packs,snacks,chocolate";
+let equipment = "space suits,jet packs,tool belts,thermal detonators";
+let pets = "parrots,cats,moose,alien eggs";
+let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
+
+//a) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
+let foodCabinet = [];
+foodCabinet = food.split(',').sort();
+let equipmentCabinet = [];
+equipmentCabinet = equipment.split(',').sort();
+let petsCabinet = []
+petsCabinet = pets.split(',').sort();
+let sleepAidsCabinet = [];
+sleepAidsCabinet = sleepAids.split(',').sort();
+
+
+
+
+//b) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
+let cargoHold = []
+cargoHold.push(foodCabinet, equipmentCabinet, petsCabinet, sleepAidsCabinet);
+console.log(cargoHold);
+console.log("\n");
+
+//c) Query the user to select a cabinet (0 - 3) in the cargoHold.
+const input = require('readline-sync');
+cabinetNumber = input.question("Select a cabinet from 0-3 in the cargoHold: ");
+cabinetNumber = Number(cabinetNumber);
+
+//d) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
+
+console.log(cargoHold[cabinetNumber]);
+
+//e) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
